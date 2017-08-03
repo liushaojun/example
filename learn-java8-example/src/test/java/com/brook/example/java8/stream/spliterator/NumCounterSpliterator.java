c4fa3ce32f0493dcb98c6bb09ff55908b22e5983
liushaojun <liushaojunvip@sina.com>
2017-08-03 14:18:21 +0800

    修改注释

--- learn-java8-example/src/test/java/com/brook/example/java8/stream/spliterator/NumCounterSpliterator.java
+++ learn-java8-example/src/test/java/com/brook/example/java8/stream/spliterator/NumCounterSpliterator.java
@@ -4,6 +4,9 @@ import java.util.Spliterator;
 import java.util.function.Consumer;
 
 /**
+ * Spliterator，类似于迭代器，用于遍历源的元素。 Spliterator API被设计为支持顺序遍历，
+ * 通过支持分解以及单元素迭代来支持有效的并行遍历。 此外，通过Spliterator访问元素的协议
+ * 被设计为比Iterator施加更小的单元素开销，并避免为hasNext()和next()分别使用单独的方法所涉及的内在竞争。
  * Spliterator 详细参考 http://blog.csdn.net/anonymousprogrammer/article/details/76034365
  */
 class NumCounterSpliterator implements Spliterator<Character> {
@@ -15,7 +18,7 @@ class NumCounterSpliterator implements Spliterator<Character> {
         this.str = str;
     }
 
-    /**
+    /** 顺序处理每个元素
      * @param action
      * @return
      */
@@ -25,6 +28,12 @@ class NumCounterSpliterator implements Spliterator<Character> {
         return currentChar < str.length();
     }
 
+    /**
+     *  这就是为Spliterator专门设计的方法，区分与普通的Iterator，该方法会把当前元素
+     *  划分一部分出去创建一个新的Spliterator作为返回，两个Spliterator变会并行执行，
+     *  如果元素个数小到无法划分则返回null
+     * @return
+     */
     @Override
     public Spliterator<Character> trySplit() {
 
@@ -50,6 +59,10 @@ class NumCounterSpliterator implements Spliterator<Character> {
         return null;
     }
 
+    /**
+     * 分割器可以通过方法提供剩余元素数量的估计。
+     * @return
+     */
     @Override
     public long estimateSize() {
         return str.length() - currentChar;
